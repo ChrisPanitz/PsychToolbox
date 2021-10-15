c93ca8c59d0b8a1a8474c1d962af4f002007d7fa
Christian Panitz <christian.panitz@uni-leipzig.de>
2021-10-15 15:55:07 -0400

    Added description and changed variable names in genFlickVec.m

--- 00_customFunctions/genFlickVec.m
+++ 00_customFunctions/genFlickVec.m
@@ -1,11 +1,14 @@
-function [flickVec] = genFlickVec(frPerCyc, frTotal, func)
-    if strcmp(func, 'box')
-        flickVec = repmat([ones(frPerCyc/2,1); zeros(frPerCyc/2,1)], ...
-                          frTotal/frPerCyc, 1)';
-    elseif strcmp(func, 'sin')
-        flickVec = repmat(sin(1.5*pi:(2*pi/frPerCyc):(3.5*pi-2*pi/frPerCyc)) / 2 + .5, ...
-                          1, frTotal/frPerCyc);
+% generates vectors of luminance/opaqueness values for flicker
+% presentation (one value per frame); takes frames per cylce, total number
+% of frames and the shape of the modulation ('box' for on/off, 'sin' for
+% sinusoidal)
+
+function [flickVec] = genFlickVec(framesPerCyc, framesTotal, shapeFunc)
+    if strcmp(shapeFunc, 'box')
+        flickVec = repmat([ones(framesPerCyc/2,1); zeros(framesPerCyc/2,1)], ...
+                          framesTotal/framesPerCyc, 1)';
+    elseif strcmp(shapeFunc, 'sin')
+        flickVec = repmat(sin(1.5*pi:(2*pi/framesPerCyc):(3.5*pi-2*pi/framesPerCyc)) / 2 + .5, ...
+                          1, framesTotal/framesPerCyc);
     end
 end
-
-
