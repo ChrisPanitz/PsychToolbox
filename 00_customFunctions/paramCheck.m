7757357ce15f434090560bf4bd7d4de2fd3b4a13
Christian Panitz <christian.panitz@uni-leipzig.de>
2021-10-15 16:01:19 -0400

    Added description of paramCheck.m

--- 00_customFunctions/paramCheck.m
+++ 00_customFunctions/paramCheck.m
@@ -1,5 +1,9 @@
+% checks the validity of some some parameters and gives error messages or
+% warnings. If parameters would result in error, terminate script and give
+% explanation. If choice of parameters is problematic but does not result
+% in error, give warning and allow user to continue with button press.
 function paramCheck(framesPerCycle, framesTotal, flickMods, breakAfterTrials, imFileList, imageSize, window)
-    % check whether framesTotal is a multiple of all framesPerCycle
+    % check whether framesTotal is a multiple of all framesPerCycle (error)
     probFreqs = NaN(1,length(framesPerCycle));
     for i = length(framesPerCycle):-1:1
         if framesPerCycle(i)/framesTotal ~= round(framesPerCycle(i)/framesTotal,0)
@@ -15,17 +19,18 @@ function paramCheck(framesPerCycle, framesTotal, flickMods, breakAfterTrials, im
         end
     end
     
-    % check whether flickMods has valid arguments
+    % check whether flickMods has valid arguments (error)
     if sum(ismember(flickMods, {'box','sin'})) < length(flickMods)
         error("only 'box' and 'sin' are allowed for genFlickVec modulation input\n");
     end
     
-    % check whether image size is empty or has two values
+    % check whether image size is empty or has two values (error)
     if ~isempty(imageSize) && length(imageSize) ~= 2
         error("imageSize parameter must be empty or contain both x and y values\n");
     end
     
     % check whether breakAfterTrials leads to equal-sized blocks of trials
+    % (warning)
     if length(imFileList)/breakAfterTrials ~= round(length(imFileList)/breakAfterTrials, 0)
         fprintf(["the total number of trials is not an integer multiple of " ...
                  "breakAfterTrials. This means that the last block will be" ...
@@ -34,7 +39,7 @@ function paramCheck(framesPerCycle, framesTotal, flickMods, breakAfterTrials, im
         KbStrokeWait;
     end
     
-    % check whether number of frames for flicker stimulus is even
+    % check whether number of frames for flicker stimulus is even (warning)
     if sum(framesPerCycle./2 == round(framesPerCycle./2,0)) < length(framesPerCycle)
         fprintf(["it is recommended to use only even numbers for frames per " ...
                  "cycle. If you're sure about this, continue with a key press\n"]);
@@ -42,6 +47,7 @@ function paramCheck(framesPerCycle, framesTotal, flickMods, breakAfterTrials, im
     end
 
     % check whether there are overlaps between frequencies and harmonics
+    % (warning)
     fRate = Screen('NominalFrameRate', window);
         frequencies = fRate./framesPerCycle;
         critFreqInd = sum(ismember(frequencies, frequencies.*2));
