8e0f0cd3c644f6f70692c717cb840f0ac51eb875
Christian Panitz <christian.panitz@uni-leipzig.de>
2021-10-15 16:48:24 -0400

    Added description of randExpoInt.m

--- 00_customFunctions/randExpoInt.m
+++ 00_customFunctions/randExpoInt.m
@@ -1,6 +1,8 @@
+% returns random interval duration in seconds, following an exponential
+% function with mu as mean of the base function (exprnd function in MATLAB) 
+% and a vector with minimal and maximum duration of interval; mu = .1 works well
 function intInSec = randExpoInt(mu, minMaxSec)
     intInSec = NaN;
-    
     while ~(intInSec >= minMaxSec(1) && intInSec <= minMaxSec(2))
         intInSec = minMaxSec(1) + exprnd(mu,1,1)*diff(minMaxSec);
     end
