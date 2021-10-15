a537cafe85eef5da928d97fb20c24b9786ba6948
Christian Panitz <christian.panitz@uni-leipzig.de>
2021-10-15 15:48:26 -0400

    Added description of compPPI function

--- 00_customFunctions/compPPI.m
+++ 00_customFunctions/compPPI.m
@@ -1,3 +1,5 @@
+% computes pixels/inch of a screen given its resolution (in pixels) & its
+% diagonal (in inch)
 function ppi = compPPI(widthPix, heightPix, diagInch)
     diagPix = sqrt(widthPix^2 + heightPix^2);
     ppi = diagPix/diagInch;
