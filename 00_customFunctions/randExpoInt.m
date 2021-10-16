15d698dfb38e3145cbd9afd06da236a62f4e4dd9
Christian Panitz <christian.panitz@uni-leipzig.de>
2021-10-16 19:03:51 -0400

    randExpoInt.m now uses rand (base) instead of exprnd (stat toolbox)

--- 00_customFunctions/randExpoInt.m
+++ 00_customFunctions/randExpoInt.m
@@ -1,9 +1,11 @@
 % returns random interval duration in seconds, following an exponential
 % function with mu as mean of the base function (exprnd function in MATLAB) 
-% and a vector with minimal and maximum duration of interval; mu = .1 works well
-function intInSec = randExpoInt(mu, minMaxSec)
+% and a vector with minimal and maximum duration of interval; the value of
+% 6.5 for computing lambda was obtained by trying out
+function intInSec = randExpoInt(minMaxSec)
     intInSec = NaN;
     while ~(intInSec >= minMaxSec(1) && intInSec <= minMaxSec(2))
-        intInSec = minMaxSec(1) + exprnd(mu,1,1)*diff(minMaxSec);
+        lambda = 6.5/diff(minMaxSec);
+        intInSec = minMaxSec(1) - log(1-rand(1,1))/lambda;
     end
 end
