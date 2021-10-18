349d0f2b92f5bbdfbc558cfcc37948d570528d88
Christian Panitz <christian.panitz@uni-leipzig.de>
2021-10-18 10:59:25 -0400

    updated value for lambda and took out unnecessary "1-"in randExpoInt.m

--- ssv4/ssv4.m
+++ ssv4/ssv4.m
@@ -289,12 +289,12 @@ end
 % returns random interval duration in seconds, following an exponential
 % function with mu as mean of the base function (exprnd function in MATLAB) 
 % and a vector with minimal and maximum duration of interval; the value of
-% 6.5 for computing lambda was obtained by trying out
+% 5 for computing lambda was obtained by trying out
 function intInSec = randExpoInt(minMaxSec)
     intInSec = NaN;
     while ~(intInSec >= minMaxSec(1) && intInSec <= minMaxSec(2))
-        lambda = 6.5/diff(minMaxSec);
-        intInSec = minMaxSec(1) - log(1-rand(1,1))/lambda;
+        lambda = 5/diff(minMaxSec);
+        intInSec = minMaxSec(1) - log(rand(1,1))/lambda;
     end
 end
 
