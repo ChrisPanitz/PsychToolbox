94c5dd929436da8f4e3804991666fce47135485b
Christian Panitz <christian.panitz@uni-leipzig.de>
2022-04-26 10:25:37 -0400

    color parameter for presFix.m

--- 00_customFunctions/presFix.m
+++ 00_customFunctions/presFix.m
@@ -1,6 +1,10 @@
 % present fixation cross; cross size is hardcoded and relative to window
 % size (pixels), duration is given in seconds
-function presFix(window, fixDurSec)
+function presFix(window, fixDurSec, crossColor)
+    if nargin < 3
+        crossColor = 0; % default color is black
+    end
+
     [winSize(1), winSize(2)] = Screen('WindowSize', window);
     winCenter = winSize ./ 2;
     
@@ -8,7 +12,7 @@ function presFix(window, fixDurSec)
     fixL = round(winSize(2)*.02, 0);
     
     % Present fixation cross
-    Screen('DrawLines', window, [-fixL fixL 0 0; 0 0 -fixL fixL], 7, 0, winCenter);
+    Screen('DrawLines', window, [-fixL fixL 0 0; 0 0 -fixL fixL], 7, crossColor, winCenter);
     Screen('Flip', window);
     WaitSecs(fixDurSec);
 end
