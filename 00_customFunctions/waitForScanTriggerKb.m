31c10882287da323e391835ebc4f539a78ef9fec
Christian Panitz <christian.panitz@uni-leipzig.de>
2022-06-08 16:29:37 -0400

    waitForScanTriggerKb.m now takes char and int input

--- 00_customFunctions/waitForScanTriggerKb.m
+++ 00_customFunctions/waitForScanTriggerKb.m
@@ -1,15 +1,19 @@
-% Written to wait for and detect MRI pulses sent via serial port; uses
-% keyboard logging commands. Logs system time when pulse is detected.
-% keys = vector of acceptable keys as string (e.g. ['a', 'b'])
-
+% Written to wait for and detect MRI triggers sent via serial port; uses
+% keyboard logging commands. Logs system time when trigger is detected.
+% keys = vector of acceptable keys as chars (e.g. ['a', 'b']) or ints ([4, 5])
 function startTime = waitForScanTriggerKb(keys)
     % create keyCode object with 256 zeros (length of KbCheck keyCode output)
     keyCode = zeros(1,256);
     
-    % translate key strings into DEC format
-    keyInd = NaN(length(keys),1);
-    for key = 1:length(keys)
-        keyInd(key) = KbName(keys(key));
+    if ischar(keys)
+        % translate key strings into DEC format
+        keyInd = NaN(length(keys),1);
+        for key = 1:length(keys)
+            keyInd(key) = KbName(keys(key));
+        end
+    else
+        % leave integers as is
+        keyInd = keys;
     end
     
     % check for "key strokes" and log them in keyCode
@@ -18,7 +22,7 @@ function startTime = waitForScanTriggerKb(keys)
         WaitSecs(0.001);
     end
 
-    % When key stroke (aka pulse) has been detected, log system time
+    % When key stroke (aka trigger) has been detected, log system time
     startTime = GetSecs;
 
 end % function
