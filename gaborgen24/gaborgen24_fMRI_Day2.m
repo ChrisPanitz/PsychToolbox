8875366eab1630afae9f5e65012e64b3c6e8b95d
Christian Panitz <christian.panitz@uni-leipzig.de>
2022-08-29 21:12:19 -0400

    Minor changes to fixation cross in Gaborgen24 Day 2 MRI

--- gaborgen24/gaborgen24_fMRI_Day2.m
+++ gaborgen24/gaborgen24_fMRI_Day2.m
@@ -266,6 +266,7 @@ try
     waitForScanTriggerKb([buttonsLeft, buttonsRight, buttonsOK]); % actually waits for any button press
 
     % wait for the first MRI pulse, log system time and send code to EEG
+    presFix(w, .010);
     timeFirstTR = waitForScanTriggerKb(TRtriggerCodes);
     io64(lptPortObj, adressesLPT, firstTRcode);
     WaitSecs(.010);
@@ -531,7 +532,11 @@ end % function
 
 % present fixation cross; cross size is hardcoded and relative to window
 % size (pixels), duration is given in seconds
-function presFix(window, fixDurSec)
+function presFix(window, fixDurSec, crossColor)
+    if nargin < 3
+        crossColor = 0; % default color is black
+    end
+
     [winSize(1), winSize(2)] = Screen('WindowSize', window);
     winCenter = winSize ./ 2;
     
@@ -539,7 +544,7 @@ function presFix(window, fixDurSec)
     fixL = round(winSize(2)*.02, 0);
     
     % Present fixation cross
-    Screen('DrawLines', window, [-fixL fixL 0 0; 0 0 -fixL fixL], 7, 0, winCenter);
+    Screen('DrawLines', window, [-fixL fixL 0 0; 0 0 -fixL fixL], 7, crossColor, winCenter);
     Screen('Flip', window);
     WaitSecs(fixDurSec);
 end
